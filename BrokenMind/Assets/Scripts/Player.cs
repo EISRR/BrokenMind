cebd0cfe25c791c593b961ebb9ffa70b0854e332
EIS <evierstov@gmail.com>
2020-11-06 18:29:44 +0900

    created project, added sprites

---
+++ BrokenMind/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 20f;
+    private Rigidbody2D rb;
+
+    new private Rigigbody2D rb;
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Horizontal")) Run();
+        if (Input.GetButtonDown("Jump")) Jump();
+    }
+
+    private void Run()
+    {
+        Vector3 direction = transform.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position * direction, speed * Time.deltaTime);
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+}
