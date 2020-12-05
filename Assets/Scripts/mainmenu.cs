801740484c0446c30e8294f745056786cd0210fb
EIS <evierstov@gmail.com>
2020-12-05 15:57:09 +0900

    added main menu, + settings

---
+++ Assets/Scripts/mainmenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+    public void Play_Game()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Exit_Game()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
